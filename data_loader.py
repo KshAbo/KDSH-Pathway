bffe294c7df220a9947fada5deb95a77f202d2f5
Kshetrimayum Abo <kshabo2004@gmail.com>
2026-01-08 20:14:21 +0530

    Data Ingestion and Preprocessing

--- data_loader.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def load_datasets():
-    train_df = pd.read_csv("data/train.csv")
-    test_df = pd.read_csv("data/test.csv")
-    return train_df, test_df
--- scoring.py
+++
@@ -1,7 +0,0 @@
-def score_label(label):
-    return {
-        "Supported": 2,
-        "Constrained": -1,
-        "Unconstrained": -1,
-        "Contradicted": -3
-    }.get(label, 0)
