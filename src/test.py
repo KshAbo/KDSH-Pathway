fac68005e00d8b4f4ee680b1e403ae91254f9202
Kshetrimayum Abo <kshabo2004@gmail.com>
2026-01-12 00:40:41 +0530

    Generate result

---
+++ src/test.py
@@ -0,0 +1,20 @@
+from indexing.novel_indexer import NovelIndexer
+from reasoner import Reasoner
+
+indexer = NovelIndexer()
+
+# IMPORTANT:
+# If you already ingested via preprocess.py, indexer must ingest again
+# OR we can add serialization later— right now index rebuild is expected
+
+indexer.ingest("In search of the castaways", "./data/Books/In Search of the Castaways.txt")
+indexer.ingest("The Count of Monte Cristo", "./data/Books/The Count of Monte Cristo.txt")
+
+reasoner = Reasoner(
+    indexer=indexer,
+    book_name="The Count of Monte Cristo",
+    top_k=3,
+    out_path="out/results.jsonl"
+)
+
+reasoner.run()
