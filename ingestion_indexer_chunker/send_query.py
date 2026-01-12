1ead5360fd86de3481dad6e88701466ac7d4586b
Parag Chimankar <141899674+Chimankarparag@users.noreply.github.com>
2026-01-12 14:08:02 +0530

    cleaned repo

--- ingestion_indexer_chunker/send_query.py
+++
@@ -1,3 +0,0 @@
-# this should have the code of getting the "claims" from train_claims.jsonl 
-# in the line 377 function in implementation.ipynb send the claims as query
-# the retrieve chunks is called in reasoning_engine
