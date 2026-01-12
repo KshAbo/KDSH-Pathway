68f73fb2bc76d4ccd0fccabf10dd3cb43c0016dc
Kshetrimayum Abo <kshabo2004@gmail.com>
2026-01-12 09:45:16 +0530

    Train results

--- src/results.py
+++ src/results.py
@@ -1,20 +1,21 @@
 from indexing.novel_indexer import NovelIndexer
 from reasoner import Reasoner
 
-indexer = NovelIndexer()
+def main():
+    indexer = NovelIndexer()
 
-# IMPORTANT:
-# If you already ingested via preprocess.py, indexer must ingest again
-# OR we can add serialization later— right now index rebuild is expected
+    # IMPORTANT:
+    # If you already ingested via preprocess.py, indexer must ingest again
+    # OR we can add serialization later— right now index rebuild is expected
 
-indexer.ingest("In search of the castaways", "./data/Books/In Search of the Castaways.txt")
-indexer.ingest("The Count of Monte Cristo", "./data/Books/The Count of Monte Cristo.txt")
+    indexer.ingest("In search of the castaways", "./data/Books/In Search of the Castaways.txt")
+    indexer.ingest("The Count of Monte Cristo", "./data/Books/The Count of Monte Cristo.txt")
 
-reasoner = Reasoner(
-    indexer=indexer,
-    book_name="The Count of Monte Cristo",
-    top_k=3,
-    out_path="out/results.jsonl"
-)
+    reasoner = Reasoner(
+        indexer=indexer,
+        book_name="The Count of Monte Cristo",
+        top_k=3,
+        out_path="out/results.jsonl"
+    )
 
-reasoner.run()
+    reasoner.run()
